4c47b7cf54e7d5f6dc90df4a5a2b93439fbb5bf8
Błażej Szadkowski <42031920+Smyrcu@users.noreply.github.com>
2022-11-13 20:26:56 +0100

    Models and database setup.

--- eTicket/Program.cs
+++ eTicket/Program.cs
@@ -1,6 +1,12 @@
+using eTicket.Data;
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
+// DbContext configuration
+builder.Services.AddDbContext<AppDbContext>(options => options.UseSqlServer(
+    builder.Configuration.GetConnectionString("DefaultConnectionString")));
 builder.Services.AddControllersWithViews();
 
 var app = builder.Build();
@@ -23,5 +29,8 @@ app.UseAuthorization();
 app.MapControllerRoute(
     name: "default",
     pattern: "{controller=Home}/{action=Index}/{id?}");
+
+// Seed database
+AppDbInitializer.Seed(app);
 
 app.Run();
