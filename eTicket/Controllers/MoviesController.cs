e81fe64157f2128f1f2cc3271d6ecc71951de736
Błażej Szadkowski <42031920+Smyrcu@users.noreply.github.com>
2022-11-15 00:24:53 +0100

    Added Controllers and Views

---
+++ eTicket/Controllers/MoviesController.cs
@@ -0,0 +1,23 @@
+﻿using eTicket.Data;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Infrastructure;
+using Microsoft.EntityFrameworkCore;
+
+namespace eTicket.Controllers
+{
+    public class MoviesController : Controller
+    {
+        private readonly AppDbContext _context;
+
+        public MoviesController(AppDbContext context)
+        {
+            _context = context;
+        }
+
+        public async Task<IActionResult> Index()
+        {
+            var allMovies = await _context.Movies.ToListAsync();
+            return View(allMovies);
+        }
+    }
+}
--- eTicket/Models/Producer.cs
+++ eTicket/Models/Producer.cs
@@ -7,8 +7,11 @@ namespace eTicket.Models
         [Key]
         public int Id { get; set; }
 
+        [Display(Name = "Profile Picture")]
         public string ProfilePictureUrl { get; set; }
+        [Display(Name = "Full Name")]
         public string FullName { get; set; }
+        [Display(Name = "Biografy")]
         public string Bio { get; set; }
 
         // Relationships
